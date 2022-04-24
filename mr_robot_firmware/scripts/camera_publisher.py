17937b7939de3a9cb53715e3797f9aa649cdb9d9
namikxgithub <namanmalik0210@gmail.com>
2022-04-25 01:37:58 +0530

    fix for camera script

--- mr_robot_firmware/scripts/camera_publisher.py
+++ mr_robot_firmware/scripts/camera_publisher.py
@@ -15,15 +15,15 @@ class CameraPublisher:
 
         self.rate = rospy.Rate(10)
         self.publish_message()
-
-        self.camera = PiCamera()
-        self.camera.resolution = (640, 480)
-        self.camera.framerate = 32
-        self.raw_capture = PiRGBArray(self.camera, size=(640, 480))
     
     def publish_message(self):
-        
-        for frame in self.camera.capture_continuous(self.raw_capture, format="bgr", use_video_port=True):
+
+        camera = PiCamera()
+        camera.resolution = (640, 480)
+        camera.framerate = 32
+        raw_capture = PiRGBArray(camera, size=(640, 480))
+
+        for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
 
             image = frame.array
             #rospy.loginfo('publishing video frame')
@@ -31,7 +31,7 @@ class CameraPublisher:
             self.pub.publish(self.br.cv2_to_imgmsg(image, "bgr8"))
 
             key = cv2.waitKey(1) & 0xFF
-            self.raw_capture.truncate(0)
+            raw_capture.truncate(0)
             if key == ord("q"):
                 break
 
