e7ddd64c8e573e58f403c6223694e856767e94d2
namikxgithub <namanmalik0210@gmail.com>
2022-04-25 20:29:08 +0530

    fixed acht and fixed latency

--- mr_robot_firmware/scripts/camera_publisher.py
+++ mr_robot_firmware/scripts/camera_publisher.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 
+
 class CameraPublisher:
     def __init__(self):
         rospy.init_node('Camera_Publisher')
@@ -13,17 +14,16 @@ class CameraPublisher:
         self.pub = rospy.Publisher('video_frames', Image, queue_size=10)
         self.br = CvBridge()
 
-        self.rate = rospy.Rate(10)
-        self.publish_message()
+        self.camera = PiCamera()
+        self.camera.resolution = (640, 480)
+        self.camera.framerate = 32
+        self.raw_capture = PiRGBArray(self.camera, size=(640, 480))
+
+        #self.rate = rospy.Rate(100)
     
     def publish_message(self):
 
-        camera = PiCamera()
-        camera.resolution = (640, 480)
-        camera.framerate = 32
-        raw_capture = PiRGBArray(camera, size=(640, 480))
-
-        for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+        for frame in self.camera.capture_continuous(self.raw_capture, format="bgr", use_video_port=True):
 
             image = frame.array
             #rospy.loginfo('publishing video frame')
@@ -31,12 +31,15 @@ class CameraPublisher:
             self.pub.publish(self.br.cv2_to_imgmsg(image, "bgr8"))
 
             key = cv2.waitKey(1) & 0xFF
-            raw_capture.truncate(0)
+            self.raw_capture.truncate(0)
+
             if key == ord("q"):
                 break
 
-            self.rate.sleep()
+            #self.rate.sleep()
 
 
 if __name__ == '__main__':
     Camera = CameraPublisher()
+
+    Camera.publish_message()
