503f8ca4f9f35d16b86fdec6ee8a501fa1c1fb03
HazorTremz <himanshu.s.rawat2001@gmail.com>
2022-05-17 04:01:41 +0530

    encoder odom  update

--- mr_robot_firmware/scripts/encoder_odom.py
+++ mr_robot_firmware/scripts/encoder_odom.py
@@ -1,102 +1,188 @@
-#!/usr/bin/env python3
+\#! /usr/bin/env python3
 
+from itertools import count
 import RPi.GPIO as GPIO
 from time import sleep
 import rospy
-import std_msgs.msg import Int16
+from std_msgs.msg import Int16
 
 
-lft_counter = 0
-rgt_counter = 0
+seq_a_lw = 0
+seq_b_lw = 0
+count_rgt_lw = 0#clk wise
+count_lft_lw = 0#anit_clk wise
+
+seq_a_rw = 0
+seq_b_rw = 0
+count_rgt_rw = 0#clk wise
+count_lft_rw = 0#anit_clk wise
+
+
+cnt_lw=0
+cnt_rw=0
 
 lft_Enc_A = 17
 lft_Enc_B = 27
 
-rgt_Enc_A = 18
-rgt_Enc_B = 28
+rgt_Enc_A = 23
+rgt_Enc_B = 24
 
-rospy.init_node("EncoderOdom", anonymous=True)
+wheel_rad = 0.094
+d_in_1_roatation = 0.5906#in meter
 
-right_pub = rospy.Publisher('right_count', Int16, queue_size=10)
-left_pub = rospy.Publisher('left_count', Int16, queue_size=10)
+fwrd_distance=0
+bkwrd_distance = 0
 
+def algo(val_a,val_b,seq_a,seq_b):
 
-def right_callback(r_data):
-    rospy.loginfo("Right_Wheel_ticks = %d\n", r_data.data)
+    seq_a= seq_a << 1    
+    seq_a= seq_a | val_a
+    seq_a = seq_a & 15
 
-def left_callback(l_data):
-    rospy.loginfo("Left_Wheel_ticks = %d\n", l_data.data)
+    seq_b = seq_b << 1
+    seq_b= seq_b | val_b
+    seq_b = seq_b & 15
+
+    return [seq_a,seq_b]
 
 def init():
     print("Rotary Encoder Test Program")
     GPIO.setwarnings(True)
     GPIO.setmode(GPIO.BCM)
 
-    right_pub.publish(rgt_counter)
-    left_pub.publish(lft_counter)
-
+    GPIO.setup(rgt_Enc_B, GPIO.IN)
+    GPIO.setup(rgt_Enc_A, GPIO.IN)
     GPIO.setup(lft_Enc_A, GPIO.IN)
     GPIO.setup(lft_Enc_B, GPIO.IN)
-    GPIO.setup(rgt_Enc_A, GPIO.IN)
-    GPIO.setup(rgt_Enc_B, GPIO.IN)
 
-    rospy.Subscriber("right_count", Int16, right_callback)
-    rospy.Subscriber("left_count", Int16, left_callback)
 
-    GPIO.add_event_detect(lft_Enc_A, GPIO.RISING, callback=lft_rotation_decode, bouncetime=10)
-    GPIO.add_event_detect(rgt_Enc_A, GPIO.RISING, callback=rgt_rotation_decode, bouncetime=10)
+    GPIO.add_event_detect(lft_Enc_B, GPIO.BOTH, callback=isr_B)
+    GPIO.add_event_detect(lft_Enc_A, GPIO.BOTH, callback=isr_A)
+
+    GPIO.add_event_detect(rgt_Enc_B, GPIO.BOTH, callback=isr_b)
+    GPIO.add_event_detect(rgt_Enc_A, GPIO.BOTH, callback=isr_a)
+
 
     return
 
 
-def lft_rotation_decode(lft_Enc_A):
-    global lft_counter
-    sleep(0.002)
-    val_A = GPIO.input(lft_Enc_A)
-    val_B = GPIO.input(lft_Enc_B)
+def isr_A(lft_Enc_A):
 
-    if (val_A == 1) and (val_B == 0):
-        lft_counter += 1
-        print("direction -> ", lft_counter)
-        while val_B == 0:
-            val_B = GPIO.input(lft_Enc_B)
-        while val_B == 1:
-            val_B = GPIO.input(lft_Enc_B)
-        return
+    global seq_a_lw
+    global seq_b_lw
+    global count_lft_lw
+    global count_rgt_lw
+    global cnt_lw
+    global fwrd_distance
+    global bkwrd_distance
 
-    elif (val_A == 1) and (val_B == 1):
-        lft_counter -= 1
-        print("direction <- ", lft_counter)
-        while val_A == 1:
-            val_A = GPIO.input(lft_Enc_A)
-        return
+    a_val = GPIO.input(lft_Enc_A)
+    b_val = GPIO.input(lft_Enc_B)
+
+    seq_a_lw= algo(a_val,b_val,seq_a_lw,seq_b_lw)[0]
+    seq_b_lw = algo(a_val,b_val,seq_a_lw,seq_b_lw)[1]
+    #anti_clk wise
+    if(seq_a_lw == 9) & (seq_b_lw==3):
+        count_lft_lw = count_lft_lw+1
+    #clk wise
+    if (seq_a_lw == 3) & (seq_b_lw ==9):
+        count_rgt_lw = count_rgt_lw + 1
+    cnt_lw = count_lft_lw-count_rgt_lw
+    #forward motion +ve
+    print(f"counter_lw:{cnt_lw}")
+    left_pub.publish(cnt_lw)
+    if  cnt_lw>0:
+        fwrd_distance = cnt_lw*d_in_1_roatation
+        print(f"foward_distance:{round(fwrd_distance,4)}")
     else:
-        return
+        bkwrd_distance =abs(cnt_lw*d_in_1_roatation)
+        print(f"backword_distance={round(bkwrd_distance,4)}")
 
+def isr_B(lft_Enc_B):
+    global seq_a_lw
+    global seq_b_lw
+    global count_lft_lw
+    global count_rgt_lw
+    global cnt_lw
 
-def rgt_rotation_decode(rgt_Enc_A):
-    global rgt_counter
-    sleep(0.002)
-    val_A = GPIO.input(rgt_Enc_A)
-    val_B = GPIO.input(rgt_Enc_B)
+    a_val = GPIO.input(lft_Enc_A)
+    b_val = GPIO.input(lft_Enc_B)
 
-    if (val_A == 1) and (val_B == 0):
-        rgt_counter += 1
-        print("direction -> ", rgt_counter)
-        while val_B == 0:
-            val_B = GPIO.input(rgt_Enc_B)
-        while val_B == 1:
-            val_B = GPIO.input(rgt_Enc_B)
-        return
+    seq_a_lw= algo(a_val,b_val,seq_a_lw,seq_b_lw)[0]
+    seq_b_lw = algo(a_val,b_val,seq_a_lw,seq_b_lw)[1]
 
-    elif (val_A == 1) and (val_B == 1):
-        rgt_counter -= 1
-        print("direction <- ", rgt_counter)
-        while val_A == 1:
-            val_A = GPIO.input(lft_Enc_A)
-        return
+    #anti_clk wise
+    if(seq_a_lw == 9) & (seq_b_lw==3):
+        count_lft_lw = count_lft_lw+1
+    #clk wise
+    if (seq_a_lw == 3) & (seq_b_lw ==9):
+        count_rgt_lw = count_rgt_lw + 1
+    cnt_lw = count_lft_lw-count_rgt_lw
+    #forward motion +ve
+    print(f"counter_lw:{cnt_lw}")
+    left_pub.publish(cnt_lw)
+
+    if  cnt_lw>0:
+        fwrd_distance = cnt_lw*d_in_1_roatation
+        print(f"foward_distance:{round(fwrd_distance,4)}")
     else:
-        return
+        bkwrd_distance =abs(cnt_lw*d_in_1_roatation)
+        print(f"backword_distance={round(bkwrd_distance,4)}")
+
+def isr_b(rgt_Enc_B):
+
+    global seq_a_rw
+    global seq_b_rw
+    global count_lft_rw
+    global count_rgt_rw
+    global cnt_rw
+    global fwrd_distance
+    global bkwrd_distance
+    a_val = GPIO.input(rgt_Enc_A)
+    b_val = GPIO.input(rgt_Enc_B)
+    print(a_val,b_val)
+
+    seq_a_rw= algo(a_val,b_val,seq_a_rw,seq_b_rw)[0]
+    seq_b_rw = algo(a_val,b_val,seq_a_rw,seq_b_rw)[1]
+    print(seq_a_rw,seq_b_rw)
+
+    #anti_clk wise
+    if(seq_a_rw == 9) & (seq_b_rw==3):
+        count_lft_rw = count_lft_rw+1
+    #clk wise
+    if (seq_a_rw == 3) & (seq_b_rw ==9):
+        count_rgt_rw = count_rgt_rw + 1
+    cnt_rw = count_lft_rw-count_rgt_rw
+    #forward motion +ve
+    print(f"counter_rw:{cnt_rw}")
+    right_pub.publish(cnt_rw)
+
+def isr_a(rgt_Enc_A):
+
+    global seq_a_rw
+    global seq_b_rw
+    global count_lft_rw
+    global count_rgt_rw
+    global cnt_rw
+
+    a_val = GPIO.input(rgt_Enc_A)
+    b_val = GPIO.input(rgt_Enc_B)
+    print(a_val,b_val)
+
+    seq_a_rw= algo(a_val,b_val,seq_a_lw,seq_b_rw)[0]
+    seq_b_rw = algo(a_val,b_val,seq_a_lw,seq_b_rw)[1]
+    print(seq_a_rw,seq_b_rw)
+
+    #anti_clk wise
+    if(seq_a_rw == 9) & (seq_b_rw==3):
+        count_lft_rw = count_lft_rw+1
+    #clk wise
+    if (seq_a_rw == 3) & (seq_b_rw ==9):
+        count_rgt_rw = count_rgt_rw + 1
+    cnt_rw = count_lft_rw-count_rgt_rw
+    #forward motion +ve
+    print(f"counter_rw:{cnt_rw}")  
+    right_pub.publish(cnt_rw)
 
 
 def main():
@@ -112,4 +198,10 @@ def main():
 
 
 if __name__ == '__main__':
+    rospy.init_node("EncoderOdom", anonymous=True)
+
+    right_pub = rospy.Publisher('right_count', Int16, queue_size=10)
+    left_pub = rospy.Publisher('left_count', Int16, queue_size=10)
+    # vel_pub = rospy.Publisher('/cmd_vel',Twist,queue_size = 10)
+ 
     main()
